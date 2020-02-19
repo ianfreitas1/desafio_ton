89caf95ad349d6080781e03b4dbc2e51bc2b8896
Ian <ianfreitas1@poli.ufrj.br>
2020-02-18 23:16:39 -0300

    Alteração no model de cards

--- desafio_ton/cards/models.py
+++ desafio_ton/cards/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from desafio_ton.wallets.models import Wallet
+
 
 class Card(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
@@ -11,5 +13,11 @@ class Card(models.Model):
     expiration_date = models.DateField(verbose_name=_('expiration date'))
     user_name = models.CharField(verbose_name=_('user name'), max_length=100)
     cvv = models.CharField(max_length=4)
-    limit = models.FloatField(verbose_name=_('limit'))
-    # wallet = models.ForeignKey(Wallet, null=True, blank=True, default=None, related_name='cards')
+    limit = models.FloatField(verbose_name=_('limit'), default=0.0)
+    wallet = models.ForeignKey(
+        Wallet, null=True, blank=True, default=None, related_name='cards', on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = 'cards'
+        verbose_name = _('card')
+        verbose_name_plural = _('cards')
