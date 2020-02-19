4573b91b5e650eaa67106448eb082b40fc91b438
Ian <ianfreitas1@poli.ufrj.br>
2020-02-19 18:43:34 -0300

    Adicionados métodos put e delete de cartões sem permissões

--- desafio_ton/cards/urls.py
+++ desafio_ton/cards/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('cards/', views.CardView.as_view(), name='cards')
+    path('cards/', views.CardView.as_view(), name='cards'),
+    path('cards/<int:card_id>/', views.CardDetailView.as_view(), name='card_detail'),
 ]
--- desafio_ton/cards/views.py
+++ desafio_ton/cards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext as _
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
@@ -14,7 +15,7 @@ class CardView(APIView):
 
         serializer = CardSerializer(cards, many=True)
 
-        return Response(serializer.data)
+        return Response({'cards': serializer.data})
 
     def post(self, request):
         data = request.data
@@ -26,7 +27,41 @@ class CardView(APIView):
                 serializer.save()
 
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
+
             except:
-                return Response("erro ao criar cartão", status=status.HTTP_400_BAD_REQUEST)
+                return Response(_('Failed to add new card.'), status=status.HTTP_400_BAD_REQUEST)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CardDetailView(APIView):
+
+    def get(self, request, card_id):
+        card = get_object_or_404(Card, id=card_id)
+
+        serializer = CardSerializer(card)
+
+        return Response(serializer.data)
+
+    def put(self, request, card_id):
+        card = get_object_or_404(Card, id=card_id)
+
+        serializer = CardSerializer(card, data=request.data)
+
+        if serializer.is_valid():
+            try:
+                serializer.save()
+
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            except:
+                return Response(_('Failed to edit card.'), status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, card_id):
+        card = get_object_or_404(Card, id=card_id)
+
+        card.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
