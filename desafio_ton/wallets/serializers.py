c123c79c9ffd9c5e8e813fc9cd230eccd8eec2a2
Ian <ianfreitas1@poli.ufrj.br>
2020-02-19 20:14:32 -0300

    Adicionados limite máximo e crédito disponível ao serializer da wallet

--- desafio_ton/wallets/serializers.py
+++ desafio_ton/wallets/serializers.py
@@ -3,7 +3,27 @@ from .models import Wallet
 
 
 class WalletSerializer(serializers.ModelSerializer):
+    max_limit = serializers.SerializerMethodField()
+    available_credit = serializers.SerializerMethodField()
 
     class Meta:
         model = Wallet
-        fields = '__all__'
+        fields = ('id', 'user', 'limit', 'max_limit', 'available_credit')
+
+    def get_max_limit(self, obj):
+        cards = obj.cards.all()
+
+        max_limit = 0
+        for card in cards:
+            max_limit += card.limit
+
+        return max_limit
+
+    def get_available_credit(self, obj):
+        cards = obj.cards.all()
+
+        available_credit = 0
+        for card in cards:
+            available_credit += card.available_credit
+
+        return available_credit
