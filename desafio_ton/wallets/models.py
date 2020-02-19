89a793b143454278504ee04b28efbea8e897ce50
Ian <ianfreitas1@poli.ufrj.br>
2020-02-18 23:16:52 -0300

    Adicionado model de wallet

--- desafio_ton/wallets/models.py
+++ desafio_ton/wallets/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Wallet(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    limit = models.FloatField(verbose_name=_('limit'), default=0.0)
